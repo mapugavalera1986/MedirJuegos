1d6a7a76e9a1d511acfe9640a4c6e65c9a63c160
Mario Augusto Puga Valera <mapugavalera@gmail.com>
2023-11-13 12:49:34 -0500

    Espero que esto funcione

--- MedirJuegos/Views/Components/JuegoCard.swift
+++ MedirJuegos/Views/Components/JuegoCard.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct JuegoCard: View {
+    //Aquí van los valores de la base de datos
+    var juego: String
+    var fecha: String
+    var tiempo: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{//Empieza la tarjeta
+            Text(fecha)
+                .font(.system(size: 20))
+                .foregroundStyle(.white)
+                .padding(2)
+            Text(juego)
+                .font(.system(size: 24, weight: .bold))
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .padding(4)
+            Text(tiempo)
+                .font(.system(size: 20))
+                .foregroundStyle(.white)
+                .padding(2)
+        }//Finaliza la tarjeta
+        .frame(width: 320, height: 160)
+        .background(Color("Especiales"))
+        .padding(4)
+        .cornerRadius(20)
     }
 }
 
 #Preview {
-    JuegoCard()
+    JuegoCard(
+        juego: "Doom 64",
+        fecha: "12-12-2022",
+        tiempo: "1 hora"
+    )
 }
